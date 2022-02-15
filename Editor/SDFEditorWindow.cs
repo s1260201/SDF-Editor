87aca7127a3fd4ffaa1cfbcdb732cc531e11f9a4
s1260201 <72482090+s1260201@users.noreply.github.com>
2022-02-15 22:48:45 +0900

    First

---
+++ Editor/SDFEditorWindow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SDFEditorWindow : EditorWindow
+{
+    private const float WINDOWSIZE_W = 500.0f;          //ウィンドウサイズ横幅
+    private const float WINDOWSIZE_H = 200.0f;          //ウィンドウサイズ縦幅
+
+    /// <summary>
+    /// ウィンドウ表示
+    /// </summary>
+    [MenuItem("Window/SDF-Editor")]
+    static void Open()
+    {
+        var window = GetWindow<SDFEditorWindow>();
+        //ウィンドウサイズ設定(minとmaxを=しているのはウィンドウサイズを固定するため)
+        window.maxSize = window.minSize = new Vector2(WINDOWSIZE_W, WINDOWSIZE_H);
+    }
+}
