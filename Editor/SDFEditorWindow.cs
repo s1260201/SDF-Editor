deffa45605dc8c7fa26d482c41e61bfaf1064687
s1260201 <72482090+s1260201@users.noreply.github.com>
2022-02-16 22:45:00 +0900

    fixing editor

--- Editor/SDFEditorWindow.cs
+++ Editor/SDFEditorWindow.cs
@@ -3,17 +3,20 @@ using UnityEditor;
 
 public class SDFEditorWindow : EditorWindow
 {
-    private const float WINDOWSIZE_W = 500.0f;          //ウィンドウサイズ横幅
-    private const float WINDOWSIZE_H = 200.0f;          //ウィンドウサイズ縦幅
-
-    /// <summary>
-    /// ウィンドウ表示
-    /// </summary>
     [MenuItem("Window/SDF-Editor")]
     static void Open()
     {
+        /*
         var window = GetWindow<SDFEditorWindow>();
         //ウィンドウサイズ設定(minとmaxを=しているのはウィンドウサイズを固定するため)
         window.maxSize = window.minSize = new Vector2(WINDOWSIZE_W, WINDOWSIZE_H);
+        */
+        //EditorWindow.GetWindow("SDFEditorWindow");
+        GetWindow<SDFEditorWindow>("SDFEditorWindow");
+    }
+
+    void OnGUI()
+    {
+        //  実際のウィンドウのコードはここに書きます
     }
 }
