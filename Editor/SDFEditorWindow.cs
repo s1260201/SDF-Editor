985e788c7514cc318f2f04056eba5e1f3f5781c4
s1260201 <72482090+s1260201@users.noreply.github.com>
2022-02-17 18:41:50 +0900

    Material preview

--- Editor/SDFEditorWindow.cs
+++ Editor/SDFEditorWindow.cs
@@ -1,8 +1,15 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
+using UnityEngine.UIElements;
+
+
 public class SDFEditorWindow : EditorWindow
 {
+    public Material material;
+    Editor gameObjectEditor;
+
+
     [MenuItem("Window/SDF-Editor")]
     static void Open()
     {
@@ -18,5 +25,15 @@ public class SDFEditorWindow : EditorWindow
     void OnGUI()
     {
         //  実際のウィンドウのコードはここに書きます
+        var label = new Label("Material");
+        material = (Material) EditorGUILayout.ObjectField(material, typeof(Material), true);
+
+        if (material != null)
+        {
+            if (gameObjectEditor == null)
+                gameObjectEditor = Editor.CreateEditor(material);
+
+            gameObjectEditor.OnPreviewGUI(GUILayoutUtility.GetRect(500, 500), EditorStyles.whiteLabel);
+        }
     }
 }
