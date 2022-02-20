f135f09b0e35bf67ea040f84979604ddb951c50d
s1260201 <72482090+s1260201@users.noreply.github.com>
2022-02-21 00:51:05 +0900

    Editing a window

--- Editor/SDFEditorWindow.cs
+++ Editor/SDFEditorWindow.cs
@@ -19,21 +19,28 @@ public class SDFEditorWindow : EditorWindow
         window.maxSize = window.minSize = new Vector2(WINDOWSIZE_W, WINDOWSIZE_H);
         */
         //EditorWindow.GetWindow("SDFEditorWindow");
-        GetWindow<SDFEditorWindow>("SDFEditorWindow");
+        var window = GetWindow<SDFEditorWindow>("SDFEditorWindow");
     }
 
     void OnGUI()
     {
         //  実際のウィンドウのコードはここに書きます
-        var label = new Label("Material");
-        material = (Material) EditorGUILayout.ObjectField(material, typeof(Material), true);
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.BeginVertical();
+            EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Material");
+                material = (Material) EditorGUILayout.ObjectField(material, typeof(Material), true);
+            EditorGUILayout.EndHorizontal();
+        
+            if (material != null)
+            {
+                if (gameObjectEditor == null)
+                    gameObjectEditor = Editor.CreateEditor(material);
 
-        if (material != null)
-        {
-            if (gameObjectEditor == null)
-                gameObjectEditor = Editor.CreateEditor(material);
-
-            gameObjectEditor.OnPreviewGUI(GUILayoutUtility.GetRect(500, 500), EditorStyles.whiteLabel);
-        }
+                gameObjectEditor.OnPreviewGUI(GUILayoutUtility.GetRect(100, 100), EditorStyles.whiteLabel);
+            }
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.LabelField("Material");
+        EditorGUILayout.EndVertical();
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SDF-Editor
 s1260201 GT
+- App or Extending the Editor ?
+
+<br>
+
+How to use
+
+1, Connecting nodes
+
+2, Exporting codes
