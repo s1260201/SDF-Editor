5252359620e352ef083796935ebd0006d76a953e
s1260201 <72482090+s1260201@users.noreply.github.com>
2022-03-04 00:53:04 +0900

    Making RaymaGraph

---
+++ Editor/src/Node modeling/SDFGraph.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using XNode;
+
+[CreateAssetMenu(fileName ="RaymaGraph", menuName ="Node Graph/RaymaGraph")]
+public class SDFGraph : NodeGraph
+{
+    
+}
---
+++ SDFNodeModeling/Nodes/BoxNode.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System;
+
+namespace RayMarching
+{
+    public class Box : XNode.Node
+    {
+        [Input] public Vector3 p;
+        [Input] public Vector3 b;
+        [Output] public float sd;
+
+        public override object GetValue(XNode.NodePort port)
+        {
+            p = GetInputValue<Vector3>("p", this.p);
+            b = GetInputValue<Vector3>("b", this.b);
+            Vector3 q;
+            q.x = Math.Max(Math.Abs(p.x) - b.x, 0.0f);
+            q.y = Math.Max(Math.Abs(p.y) - b.y, 0.0f);
+            q.z = Math.Max(Math.Abs(p.z) - b.z, 0.0f);
+
+            sd = q.magnitude + Math.Min(Math.Max(q.x, Math.Max(q.y, q.z)), 0.0f);
+            return sd;
+        }
+    }
+}
+
